1bc02500d95fb6dc0912aab2e158e1bff2e3fdfc
AhmadFreeMan <ahmadreza.mirzaei03@gmail.com>
2025-12-06 13:48:49 +0330

    feat: add basic registerNewAnswer logic with prompt input and activate poll button

--- script.js
+++ script.js
@@ -5,4 +5,17 @@ const poll = {
   options: ["0: JavaScript", "1: Python", "2: Rust", "3: C++"],
   answers: new Array(4).fill(0),
   // [0,0,0,0]
+  registerNewAnswer: function () {
+    console.log(`${this.question}
+${this.options[0]}
+${this.options[1]}
+${this.options[2]}
+${this.options[3]}  
+`);
+    const registerInput = prompt("(write your option number)");
+  },
 };
+
+document
+  .querySelector(".poll")
+  .addEventListener("click", poll.registerNewAnswer.bind(poll));
